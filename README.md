74ac8705e5bcacf19686fe84dcbb0abc15221248
apynu <apynu@duck.com>
2024-12-21 18:10:11 +0100

    hashing

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # url_shortener
 url_shortener that i used to get back into rust
+
+## TODO:
+- [x] hash urls
+- [ ] csv file handling
+- [ ] webserver/redirectserver
+- [ ] website
+
+## basic program flow
+- give url in webview
+- url is hashed and a url-shortener link with the hashed url at the end is returned
+- url is stored right next to the url-shortener link in a csv file
+- if url-shortener link is pressed, the program checks if the link is in the csv file, if so it redirects to the corresponding full-url-link
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+#[derive(Hash, PartialEq, Eq)]
+struct URL{
+    url: String,
+    hash: u64,
+}
+
+impl Default for URL{
+    fn default() -> Self {
+        URL{
+            url: "".to_string(),
+            hash: 0,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // create new hasher
+    let mut o_default_hasher_1 = DefaultHasher::new();
+    let mut o_default_hasher_2 = DefaultHasher::new();
+
+    let mut st_test_url_1: URL = url_builder("www.google.com".to_string());
+    let mut st_test_url_2: URL = url_builder("www.github.com".to_string());
+    
+    // create hash
+    st_test_url_1.hash(&mut o_default_hasher_1);
+    st_test_url_2.hash(&mut o_default_hasher_2);
+    
+    st_test_url_1.hash = o_default_hasher_1.finish(); 
+    st_test_url_2.hash = o_default_hasher_2.finish(); 
+}
+
+
+/// builds the url struct
+fn url_builder(url: String) -> URL{
+   URL { url, ..Default::default()} 
 }
